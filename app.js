c4b029f4a6a2f962e6f3bfdfbedf22af225f83b7
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2025-07-06 12:48:25 -0300

    feat: set up Morgan

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
-  next();
-});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.json({
