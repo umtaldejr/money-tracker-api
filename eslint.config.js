34061074106feed3ff91418ecb8b5cde15c3e4f2
Exupério Lédo Silva Júnior <uspjr.95@hotmail.com>
2025-07-06 12:39:34 -0300

    chore: configure ESLint

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    ignores: ['node_modules/', 'package-lock.json', 'coverage/']
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+      globals: {
+        ...require('globals').node
+      }
+    },
+    rules: {
+      'eol-last': ['error', 'always'],
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'no-trailing-spaces': 'error',
+      'no-undef': 'error',
+      'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    }
+  },
+  {
+    files: ['**/*.test.js', '**/*.spec.js', '__tests__/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...require('globals').jest
+      }
+    }
+  }
+];
